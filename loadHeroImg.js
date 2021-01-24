9f9abcfe4368d80d40a226b0e9ee3a176d29a268
Robert Ligman <verxus123@gmail.com>
2021-01-24 14:20:08 +0100

    fix source address in loadHeroImg.js

--- loadHeroImg.js
+++ loadHeroImg.js
@@ -1,8 +1,8 @@
 const heroImg = document.querySelector("#hero__img");
 const checkHeroImg = () => {
   if (document.body.clientWidth > 1280) {
-    heroImg.src = "/assets/3.png";
-  } else heroImg.src = "/assets/3-mobile.png";
+    heroImg.src = "assets/3.png";
+  } else heroImg.src = "assets/3-mobile.png";
 };
 checkHeroImg();
 window.onresize = checkHeroImg;
